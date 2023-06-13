4443025236f2e7549a18ebe6c9875a14050b6f94
OutcastR97 <sadman.rashiq.dbz@gmail.com>
2023-06-13 20:07:39 +0600

    userlist

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,13 +1,53 @@
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 import Tab from "@mui/material/Tab";
 import Tabs from "@mui/material/Tabs";
+import TextField from "@mui/material/TextField";
 import * as React from "react";
+import { AdminUserList, EmployeeUserList } from "../UserList/UserList";
 
 export default function ColorTabs() {
   const [value, setValue] = React.useState(0);
+  const [searchValue, setSearchValue] = React.useState("");
+  const [userList, setUserList] = React.useState([]);
+
+  React.useEffect(() => {
+    fetchUserList();
+  }, []);
+
+  const fetchUserList = async () => {
+    try {
+      const response = await fetch(
+        "http://59.152.62.177:8085/api/Employee/EmployeeData"
+      );
+      const data = await response.json();
+      setUserList(data.readEmployeeData);
+    } catch (error) {
+      console.error("Error fetching user list:", error);
+    }
+  };
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
+  };
+
+  const handleSearchChange = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  const renderUserList = () => {
+    if (value === 0) {
+      const filteredUserList = userList.filter(
+        (user) => user.employeeType === "Admin"
+      );
+      return <AdminUserList userList={filteredUserList} />;
+    } else if (value === 1) {
+      const filteredUserList = userList.filter(
+        (user) => user.employeeType === "Employee"
+      );
+      return <EmployeeUserList userList={filteredUserList} />;
+    }
+    return null;
   };
 
   return (
@@ -23,6 +63,31 @@ export default function ColorTabs() {
         <Tab label="User" sx={{ fontWeight: "bold" }} />
         <Tab label="Employee" sx={{ fontWeight: "bold" }} />
       </Tabs>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          margin: "20px",
+        }}
+      >
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => console.log("Add User clicked")}
+        >
+          Add User
+        </Button>
+        <TextField
+          id="search-input"
+          label="Search..."
+          variant="outlined"
+          size="small"
+          value={searchValue}
+          onChange={handleSearchChange}
+        />
+      </Box>
+      {renderUserList()}
     </Box>
   );
 }
---
+++ src/components/UserList/UserList.js
@@ -0,0 +1,71 @@
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import * as React from "react";
+
+export function AdminUserList({ userList }) {
+  return (
+    <Box sx={{ margin: "20px" }}>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="Admin User List">
+          <TableHead>
+            <TableRow>
+              <TableCell>Employee ID</TableCell>
+              <TableCell>First Name</TableCell>
+              <TableCell>Last Name</TableCell>
+              <TableCell>Division</TableCell>
+              <TableCell>District</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {userList.map((user) => (
+              <TableRow key={user.empID}>
+                <TableCell>{user.empID}</TableCell>
+                <TableCell>{user.firstName}</TableCell>
+                <TableCell>{user.lastName}</TableCell>
+                <TableCell>{user.disvision.trim()}</TableCell>
+                <TableCell>{user.district.trim()}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
+}
+
+export function EmployeeUserList({ userList }) {
+  return (
+    <Box sx={{ margin: "20px" }}>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="Employee User List">
+          <TableHead>
+            <TableRow>
+              <TableCell>Employee ID</TableCell>
+              <TableCell>First Name</TableCell>
+              <TableCell>Last Name</TableCell>
+              <TableCell>Division</TableCell>
+              <TableCell>District</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {userList.map((user) => (
+              <TableRow key={user.empID}>
+                <TableCell>{user.empID}</TableCell>
+                <TableCell>{user.firstName}</TableCell>
+                <TableCell>{user.lastName}</TableCell>
+                <TableCell>{user.disvision.trim()}</TableCell>
+                <TableCell>{user.district.trim()}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
+}
